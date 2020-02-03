9a1bf6a3403ce57227ec74375a239c5f1f2479b0
Ramal Yusifov <algieparvin@yahoo.com>
2020-02-03 20:46:43 +0200

    Refactored reverse operation in game model layer

--- app/src/main/java/algie/parvin/othello/model/Game.kt
+++ app/src/main/java/algie/parvin/othello/model/Game.kt
@@ -1,6 +1,5 @@
 package algie.parvin.othello.model
 
-import kotlin.math.max
 import kotlin.math.min
 
 const val BLACK = 'B'
@@ -11,11 +10,10 @@ val CHIPS = charArrayOf(BLACK, WHITE)
 class Game(val boardSize: Int = 8) {
 
     private var turn = WHITE
-    var initialPosition: Array<CharArray>
+    var initialPosition = Array(boardSize) { CharArray(boardSize) }
     var position: Array<CharArray>
 
     init {
-        initialPosition = Array(boardSize) { CharArray(boardSize) }
         position = Array(boardSize) { CharArray(boardSize) }
     }
 
@@ -27,156 +25,47 @@ class Game(val boardSize: Int = 8) {
         turn = oppositeColor(turn)
     }
 
-
-    // refactor this shit !!!
-    fun isSquareFree(row: Int, column: Int): Boolean {
-        if (row >= boardSize || column >= boardSize) {
-            return false
-        }
-        return position[row][column] !in CHIPS
-    }
-
-    private fun reverseChipsInColumn(rowStart: Int, rowEnd: Int, column: Int) {
-        for (i in rowStart until rowEnd) {
-            position[i][column] = turn
-        }
-    }
-
-    private fun reverseChipsInRow(row: Int, columnStart: Int, columnEnd: Int) {
-        for (i in columnStart until columnEnd) {
-            position[row][i] = turn
-        }
-    }
-
-    private fun reverseChipsInMainDiagonal(rowStart: Int, rowEnd: Int, colStart: Int, colEnd: Int) {
-        var i = rowStart + 1
-        var j = colStart + 1
-        while (i < rowEnd && j < colEnd) {
-            position[i][j] = turn
-            i++
-            j++
-        }
-    }
-
-    private fun reverseChipsInAntiDiagonal(rowStart: Int, rowEnd: Int, colStart: Int, colEnd: Int) {
-        var i = rowStart + 1
-        var j = colStart - 1
-        while (i < rowEnd && j > colEnd) {
-            position[i][j] = turn
-            i++
-            j--
-        }
-    }
-
-    private fun updateChipsInColumn(row: Int, column: Int) {
-        for (i in row + 1 until boardSize) {
-            if (!(position[i][column] in CHIPS)) {
+    private fun reverseChipsInRange(chips: List<IntArray>) {
+        for (indices in chips) {
+            if (position[indices[0]][indices[1]] == turn) {
                 break
             }
-            if (position[i][column] == turn) {
-                reverseChipsInColumn(row, i, column)
-                break
-            }
-        }
-
-        for (i in row - 1 downTo 0 step 1) {
-            if (!(position[i][column] in CHIPS)) {
-                break
-            }
-            if (position[i][column] == turn) {
-                reverseChipsInColumn(i, row, column)
-                break
-            }
-        }
-    }
-
-    private fun updateChipsInMainDiagonal(row: Int, column: Int) {
-        var i = row + 1
-        var j = column + 1
-        while (i < boardSize && j < boardSize) {
-            if (!(position[i][j] in CHIPS)) {
-                break
-            }
-            if (position[i][j] == turn) {
-                reverseChipsInMainDiagonal(row, i, column, j)
-                break
-            }
-            i++
-            j++
-        }
-
-        i = row - 1
-        j = column - 1
-        while (i >= 0 && j >= 0) {
-            if (!(position[i][j] in CHIPS)) {
-                break
-            }
-            if (position[i][j] == turn) {
-                reverseChipsInMainDiagonal(i, row, j, column)
-                break
-            }
-            i--
-            j--
-        }
-    }
-
-    private fun updateChipsInAntiDiagonal(row: Int, column: Int) {
-        var i = row + 1
-        var j = column - 1
-        while (i < boardSize && j >= 0) {
-            if (!(position[i][j] in CHIPS)) {
-                break
-            }
-            if (position[i][j] == turn) {
-                reverseChipsInAntiDiagonal(row, i, column, j)
-                break
-            }
-            i++
-            j--
-        }
-
-        i = row - 1
-        j = column + 1
-        while (i >= 0 && j < boardSize) {
-            if (!(position[i][j] in CHIPS)) {
-                break
-            }
-            if (position[i][j] == turn) {
-                reverseChipsInAntiDiagonal(i, row, j, column)
-                break
-            }
-            i--
-            j++
-        }
-    }
-
-    private fun updateChipsInRow(row: Int, column: Int) {
-        for (i in column + 1 until boardSize) {
-            if (!(position[row][i] in CHIPS)) {
-                break
-            }
-            if (position[row][i] == turn) {
-                reverseChipsInRow(row, column, i)
-                break
-            }
-        }
-
-        for (i in column - 1 downTo 0 step 1) {
-            if (!(position[row][i] in CHIPS)) {
-                break
-            }
-            if (position[row][i] == turn) {
-                reverseChipsInRow(row, i, column)
-                break
-            }
+            position[indices[0]][indices[1]] = turn
         }
     }
 
     private fun updateChips(row: Int, column: Int) {
-        updateChipsInColumn(row, column)
-        updateChipsInRow(row, column)
-        updateChipsInMainDiagonal(row, column)
-        updateChipsInAntiDiagonal(row, column)
+        if (isMoveValidInRange(row, column, (column-1 downTo 0).map { position[row][it] })) {
+            reverseChipsInRange((column-1 downTo 0).map { intArrayOf(row, it) })
+        }
+        if (isMoveValidInRange(row, column, (column+1 until boardSize).map { position[row][it] })) {
+            reverseChipsInRange((column+1 until boardSize).map { intArrayOf(row, it) })
+        }
+        if (isMoveValidInRange(row, column, (row+1 until boardSize).map { position[it][column] })) {
+            reverseChipsInRange((row+1 until boardSize).map { intArrayOf(it, column) })
+        }
+        if (isMoveValidInRange(row, column, (row-1 downTo 0).map { position[it][column] })) {
+            reverseChipsInRange((row-1 downTo 0).map { intArrayOf(it, column) })
+        }
+        if (isMoveValidInRange(row, column, (1..min(row, column)).map { position[row - it][column - it] })) {
+            reverseChipsInRange((1..min(row, column)).map { intArrayOf(row - it, column - it) })
+        }
+        if (isMoveValidInRange(row, column, (1..min(boardSize - 1 - row, boardSize - 1 - column)).map { position[row + it][column + it] })) {
+            reverseChipsInRange((1..min(boardSize - 1 - row, boardSize - 1 - column)).map { intArrayOf(row + it, column + it) })
+        }
+        if (isMoveValidInRange(row, column, (1..min(boardSize - row - 1, column)).map { position[row + it][column - it] })) {
+            reverseChipsInRange((1..min(boardSize - row - 1, column)).map { intArrayOf(row + it, column - it) })
+        }
+        if (isMoveValidInRange(row, column, (1..min(row, boardSize - 1 - column)).map { position[row - it][column + it] })) {
+            reverseChipsInRange((1..min(row, boardSize - 1 - column)).map { intArrayOf(row - it, column + it) })
+        }
+    }
+
+    fun isSquareFree(row: Int, column: Int): Boolean {
+        if (row < 0 || row >= boardSize || column < 0 || column >= boardSize) {
+            return false
+        }
+        return position[row][column] !in CHIPS
     }
 
     fun whiteChips(): List<IntArray> {
